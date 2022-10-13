0360b6b153ef8e71e48a1b9556c1cf361251f088
rajprince320 <rajprince320@gmail.com>
2022-10-13 13:44:30 +0530

    javascript file added

---
+++ JavaScript/Projects/RegistrationFormValidation/index.js
@@ -0,0 +1,10 @@
+function validate() {
+	let firstName = document.getElementById("first-name").value;
+	let lastName = document.getElementById("last-name").value;
+	let email = document.getElementById("email").value;
+	let city = document.getElementById("city").value;
+	let state = document.getElementById("state").value;
+	let zip = document.getElementById("zip").value;
+	let TnC = document.getElementById("t-and-c").value;
+	console.log(firstName, lastName, email, city, state, zip, TnC);
+}
