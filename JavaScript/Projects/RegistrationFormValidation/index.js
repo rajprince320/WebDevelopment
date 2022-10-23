5124278b3e8f8bfb0cf28f0ecd42142039f82c3c
rajprince320 <rajprince320@gmail.com>
2022-10-23 13:50:52 +0530

    learn many things

--- JavaScript/Projects/RegistrationFormValidation/index.js
+++ JavaScript/Projects/RegistrationFormValidation/index.js
@@ -48,4 +48,52 @@ function validate() {
 		document.getElementById("email-valid").style.display = "none";
 		document.getElementById("email-invalid").style.display = "block";
 	}
+
+	if (city.length >= 3) {
+		document.getElementById("city-invalid").style.display = "none";
+		document.getElementById("city-valid").style.display = "block";
+	} else {
+		document.getElementById("city-valid").style.display = "none";
+		document.getElementById("city-invalid").style.display = "block";
+	}
+
+	/*
+	Zip
+	- length = 6
+	- All Numbers
+
+	// parseInt - returns Number equivalent of String, if can't returns not a number
+	// isNaN - returns true if value is NaN
+	// !isNaN - returns it is number
+	
+	*/
+	let zipNumber = parseInt(zip);
+
+	if (zip.length === 6 && !isNaN(zipNumber)) {
+		document.getElementById("zip-invalid").style.display = "none";
+		document.getElementById("zip-valid").style.display = "block";
+	} else {
+		document.getElementById("zip-valid").style.display = "none";
+		document.getElementById("zip-invalid").style.display = "block";
+	}
+
+	/**
+	 * State
+	 
+
+	 */ if (state != "None") {
+		document.getElementById("state-invalid").style.display = "none";
+		document.getElementById("state-valid").style.display = "block";
+	} else {
+		document.getElementById("state-valid").style.display = "none";
+		document.getElementById("state-invalid").style.display = "block";
+	}
+	/*
+	t and c
+	*/
+	if (TnC) {
+		document.getElementById("tnc-invalid").style.display = "none";
+	} else {
+		document.getElementById("tnc-invalid").style.display = "block";
+	}
 }
